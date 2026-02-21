ce13d6cd6d9c4e4e89fcbecfba960358f8b0342b
Matias Gonzalez Bonilla <mgonzalezb01@libertadores.edu.co>
2026-02-21 20:34:24 +0000

    Agregar definición inicial de la clase Puerta con atributos y métodos

---
+++ actions.js
@@ -0,0 +1,7 @@
+quiero crear una clase sobre una Puerta 
+    objeto Puerta
+    atributos cafe madera automatica 
+    Metodos 
+    abrir o cerrar dependiendo del dia y hora especifica 
+    la puerta cuando este cerrada  tenga un aviso de que esta cerrada y muestre dia y horario cuando este abiert
+
